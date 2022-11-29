e8c9a6f3edc4e9e7276825ab1151c547fa95a72f
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-11-29 22:15:07 +0300

    Temel kavram, Atomlar ve Metod kavramı

---
+++ src/eclipse-workspace/Sample/src/orhn/App.java
@@ -0,0 +1,8 @@
+package orhn;
+
+public class App {
+	public static void main(String [] args)
+	{
+		System.out.println("Hello, World");
+	}
+}
--- src/src-console/orhn/App.java
+++ src/src-console/orhn/App.java
@@ -3,5 +3,31 @@ package orhn;
 class App{
     public static void main(String[] args) {
         System.out.println("Hello, World");    
+        Sample.foo();
+        Sample.bar();
+        Mample.tar();
+        System.out.println("Goodbye, World");
+    }
+}
+
+class Sample{
+    public static void foo()
+    {
+        System.out.println("Sample.foo");
+        Mample.tar();
+    }
+
+    public static void bar()
+    {
+        System.out.println("Sample.bar");
+        foo();
+    }
+}
+
+
+class Mample {
+    public static void tar()
+    {
+        System.out.println("Mample.tar");
     }
 }
