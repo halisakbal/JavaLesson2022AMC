c323bf810700e964a6b31f843c9630a8c53577b1
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-12-01 22:17:53 +0300

    Değişken tanımlama, değişken isimlendirme, local variable ve temel kavramlara ilişkin konular

--- src/eclipse-workspace/Sample/src/orhn/App.java
+++ src/eclipse-workspace/Sample/src/orhn/App.java
@@ -1,8 +1,26 @@
+/*
+ * Not: Klavyeden temel türden degerler okuma yöntemlerinin tam anlamı ileride detaylı olarak ele
+ * alınacak. Bu sebeple bunları bir kalıp olarak görüp yalnıca kalyeden okuma kısmı ile ilgilenecez.
+ * 
+ * 
+ * Klavyeden int türden değer okunması kalıbı
+ * 
+ */
+
 package orhn;
 
 public class App {
 	public static void main(String [] args)
 	{
-		System.out.println("Hello, World");
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		
+		System.out.print("Bir sayi giriniz:");
+		int val = Integer.parseInt(kb.nextLine());
+		
+		int result = val * val;
+		
+		System.out.println(result);
+		
 	}
 }
+
