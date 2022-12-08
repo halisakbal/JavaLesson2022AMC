66535b41ee42cb48317eebda352af6f19891bda6
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-12-08 22:13:40 +0300

    Sabitler ve Math sınıfı ve printf formatlama

--- src/eclipse-workspace/Sample/src/orhn/App.java
+++ src/eclipse-workspace/Sample/src/orhn/App.java
@@ -1,24 +1,13 @@
 /*
- * printf metodunda tamsayılar için hizalam yapılabilir
- * 
+ * Tek  tırnak karater sabiti ters bölü ile yazılmalıdır
  */
-
 package orhn;
 
 public class App {
 	public static void main(String [] args)
 	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Gün : ");
-		int day = Integer.parseInt(kb.nextLine());
-		System.out.print("Ay : ");
-		int mon = Integer.parseInt(kb.nextLine());
-		System.out.print("Yıl : ");
-		int year = Integer.parseInt(kb.nextLine());
+		char ch = '\'';
 		
-		
-		System.out.printf("%2d/%02d/%4d%n", day, mon, year);
-
+		System.out.println(ch);
 	}
 }
-
