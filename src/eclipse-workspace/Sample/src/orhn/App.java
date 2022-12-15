ec902d101cbd96d0df46d2f047d2fb21340e65ed
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-12-15 21:16:01 +0300

    operatorler ve deyimler--if deyimi

--- src/eclipse-workspace/Sample/src/orhn/App.java
+++ src/eclipse-workspace/Sample/src/orhn/App.java
@@ -1,16 +1,25 @@
 /*
- * ++ ve -- operatörlerinin operandlarının değişken olması gerekir. Aksi durumda error oluşur.
- * 
+	yukarıdaki kod böyle yazılmalıydı if lerin birbiri ile ilişkili oldugundan 
  */
+
 package orhn;
 
 public class App {
 	public static void main(String [] args)
 	{
-		int a;
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Bir sayi giriniz : ");
+		int val = Integer.parseInt(kb.nextLine());
 		
-		a = --2;//error
+		if(val > 0)//***
+			System.out.printf("%d sayısı pozitiftir%n", val);
+		else if(val == 0)
+			System.out.println("Sıfır sayısını girdiniz");
+		else if(val < 0)
+			System.out.printf("%d sayısı negatiftir%n", val);
 		
 		
+		System.out.println("Tekrar yapiyormusun ALiiii");
+
 	}
 }
