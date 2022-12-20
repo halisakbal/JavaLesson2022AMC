e3c3d14793a769da90146f82bd6ef1232cf0b9a8
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-12-20 22:06:39 +0300

    while dongu deyimi

--- src/eclipse-workspace/Sample/src/orhn/App.java
+++ src/eclipse-workspace/Sample/src/orhn/App.java
@@ -1,25 +1,33 @@
 /*
-	yukarıdaki kod böyle yazılmalıydı if lerin birbiri ile ilişkili oldugundan 
- */
+ *   Kontrolün sonra yapıldığı while döngü deyiminin (do-while döngüsü) genel biçimi:
+	 
+	 do
+	 	<deyim>
+	 while (<boolean türden ifade>);
+	 
+	 Akış do-while döngüsüne geldiğinde koşul kontrolü yapılmaz. İlk adım çalıştırıldıktan sonra kontrol yapılmaya başlar.
+	 Yani bu döngü deyiminin en az bir kez çalıştırılacağı garanti altındadır. while döngü deyimi kadar sık kullanılmasa da
+	 okunabilirliği/algılanabilirliği artırmak ve algoritmanın sadeleştirilmesi için tercih edilebilir. Ancak bu döngü
+	 deyimi gereksiz yere kullanıldığında okunabilirliği/algılanabilirliği azaltır. Buna dikkat edilmelidir
 
+ */
 package orhn;
 
 public class App {
 	public static void main(String [] args)
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Bir sayi giriniz : ");
+		System.out.print("Bir sayı giriniz:");
 		int val = Integer.parseInt(kb.nextLine());
 		
-		if(val > 0)//***
-			System.out.printf("%d sayısı pozitiftir%n", val);
-		else if(val == 0)
-			System.out.println("Sıfır sayısını girdiniz");
-		else if(val < 0)
-			System.out.printf("%d sayısı negatiftir%n", val);
+		int i = 0;
 		
+		do {
+			System.out.printf("%d ", i);
+			++i;
+		} while (i < val);
 		
-		System.out.println("Tekrar yapiyormusun ALiiii");
-
+		System.out.printf("%nDöngü sonrası :i =  %d%n", i);
+		
 	}
 }
