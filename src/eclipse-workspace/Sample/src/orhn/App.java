6358d10a2265c69f08ab8ffe5d30160547bbb35a
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-12-27 21:37:31 +0300

    for dongu deyimi

--- src/eclipse-workspace/Sample/src/orhn/App.java
+++ src/eclipse-workspace/Sample/src/orhn/App.java
@@ -1,33 +1,12 @@
 /*
- *   Kontrolün sonra yapıldığı while döngü deyiminin (do-while döngüsü) genel biçimi:
-	 
-	 do
-	 	<deyim>
-	 while (<boolean türden ifade>);
-	 
-	 Akış do-while döngüsüne geldiğinde koşul kontrolü yapılmaz. İlk adım çalıştırıldıktan sonra kontrol yapılmaya başlar.
-	 Yani bu döngü deyiminin en az bir kez çalıştırılacağı garanti altındadır. while döngü deyimi kadar sık kullanılmasa da
-	 okunabilirliği/algılanabilirliği artırmak ve algoritmanın sadeleştirilmesi için tercih edilebilir. Ancak bu döngü
-	 deyimi gereksiz yere kullanıldığında okunabilirliği/algılanabilirliği azaltır. Buna dikkat edilmelidir
-
+ *  for döngüsünde ikinci kısmın boş bırakılması durumunda sonsuz döngü oluşur. Yani koşul her zaman gerçeklenir
  */
 package orhn;
 
 public class App {
 	public static void main(String [] args)
-	{
-		java.util.Scanner kb = new java.util.Scanner(System.in);
-		System.out.print("Bir sayı giriniz:");
-		int val = Integer.parseInt(kb.nextLine());
-		
-		int i = 0;
-		
-		do {
-			System.out.printf("%d ", i);
-			++i;
-		} while (i < val);
-		
-		System.out.printf("%nDöngü sonrası :i =  %d%n", i);
-		
+	{	
+		for(;;)
+			;
 	}
 }
