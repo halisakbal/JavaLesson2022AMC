c4941a5ba81fcc4d61947180b38f0c3d97bd1005
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-12-29 23:12:22 +0300

    for, break ...

--- src/eclipse-workspace/Sample/src/orhn/App.java
+++ src/eclipse-workspace/Sample/src/orhn/App.java
@@ -1,12 +1,63 @@
-/*
- *  for döngüsünde ikinci kısmın boş bırakılması durumunda sonsuz döngü oluşur. Yani koşul her zaman gerçeklenir
- */
+/*----------------------------------------------------------------------------------------------------------------------	 
+	switch deyiminin genel biçimi:
+	switch (<ifade>) {
+		case <sabit ifadesi>:
+			<deyim>
+		case <sabit ifadesi>:
+			<deyim>
+			
+		...
+		
+		[
+		default:
+			<deyim>
+		]			
+	}
+	
+	Bu deyimde parantez içerisindeki ifade temel türler için tamsayı türlerinden biri veya char türünden olabilir.
+	Temel türler dışında String veya enum türlerinden de olabilir. Bu türler ileride ele alınacaktır. switch 
+	deyiminin default kısmı zorunlu değildir.
+	
+	
+	Anahrtat Notlar: switch deyimi Java 12'den itibaren ifade (expression) olarak da kullanılabilmektedir. Bu tarz 
+	deyimlere/ifadelere "ifadesel deyim (expression statement)". switch'in ifade olarak kullanılmasına yönelik sentaks
+	ve semantik eklentiler yapılmıştır. İleride ele alınacaktır.  
+----------------------------------------------------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------------------------------------------------	 
+	Aşağıdaki switch deyiminin if deyimi karşılığı:
+	
+	if (postalCode == 34387)
+		System.out.println("Mecidiyeköy");
+	else if (postalCode ==  67100)
+		System.out.println("Zonguldak Bahçelievler");
+	else
+		System.out.println("Geçersiz posta kodu");
+----------------------------------------------------------------------------------------------------------------------*/
+
 package orhn;
 
 public class App {
 	public static void main(String [] args)
-	{	
-		for(;;)
-			;
+	{
+		java.util.Scanner kb = new java.util.Scanner(System.in);
+		System.out.print("Posta kodunu giriniz:");
+		
+		int postalCode = Integer.parseInt(kb.nextLine());
+		
+		switch (postalCode) {
+		case 414100:
+			System.out.println("Gebze");
+			break;
+		case 37100:
+			System.out.println("Kastamonu Merkez");
+			break;
+		default:
+			System.out.println("Geçersiz posta kodu");
+		}
+		
+		System.out.println("Tekrar yapıyor muyuz ALİ?");	
 	}
 }
+
+
