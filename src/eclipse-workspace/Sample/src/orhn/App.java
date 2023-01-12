08fac08e82d942ba3d183d8f72cbb5e4b6d83b2c
OrhanOZTURK <orhn.ozturk@gmail.com>
2023-01-12 23:32:02 +0300

    Nesne Yonelimli Programlamaya giriş. Sınıfın Member field'ları ele alındı

--- src/eclipse-workspace/Sample/src/orhn/App.java
+++ src/eclipse-workspace/Sample/src/orhn/App.java
@@ -1,11 +1,6 @@
-
 /*----------------------------------------------------------------------------------------------------------------------	 
- 	1.Aday metotlar : 1, 2, 3, 4
- 	2. Uygun metotlar: 1, 2, 3, 4
- 	3. En uygun metot: 1
- 	
- 	
- 	T1 ->T2 T1-> T3------  T2 -> T3 ve T2 ye donusmuyor T2
+	Sınıfın non-static bir veri elamanına sınıf dışından sınıf ismi ile erişilemez. Çünkü non-static veri elemanı
+	nesneye özeldir
 ----------------------------------------------------------------------------------------------------------------------*/
 
 package orhn;
@@ -13,40 +8,15 @@ package orhn;
 class App {	
 	public static void main(String [] args)
 	{
-		short a = 10;
+		Sample.a = 10; // non static oldugu için gecersiz
 		
-		MyMath.abs(a); 
-
 	}
 }
 
-class MyMath{
-	public static int abs(int a)  //#1
-	{
-		System.out.println("MyMath.abs int");
-		return a < 0 ? -a : a;
-	}
-	
-	public static double abs(double a)  //#2
-	{
-		System.out.println("MyMath.abs double");
-		
-		return a < 0 ? -a : a;
-	}
-		
-	public static float abs(float a)  //#3
-	{
-		System.out.println("MyMath.abs float");
-		
-		return a < 0 ? -a : a;
-	}
-	
-	public static long abs(long a)  //#4
-	{
-		System.out.println("MyMath.abs long");
-		
-		return a < 0 ? -a : a;
-	}
+
+class Sample{
+	public int a;
+	public boolean b;
 }
 
 
