cd5ab2aa3f2136e8fc990e2c7587178475e196bb
OrhanOZTURK <orhn.ozturk@gmail.com>
2023-02-09 22:06:12 +0300

    String ve Character sınıfları

--- src/eclipse-workspace/Sample/src/orhn/App.java
+++ src/eclipse-workspace/Sample/src/orhn/App.java
@@ -1,97 +1,61 @@
 /*----------------------------------------------------------------------------------------------------------------------	 
-	Sınıf Çalışması: Klavyeden kullanıcı adı ve şifre isteyen basit bir ATM uygulamasının bir parçasını aşağıdaki
-	açıklmalara göre yazınız.
-	Açıklamalar:
-	- Kullanıcı ad ve şifresi en fazla 3(üç) kez denenebilecektir.
-	
-	- Denemeler sırasında doğru giriş yapılırsa "Giriş başarılı", başarısız denemeler "Giriş başarız" yazısı basılacaktır
-	
-	- En fazla 3 deneme sonucunde başarısız olunmuşsa "Giriş başarısız. Artık deneme hakkınız biti!..." yazısı basılacaktır
-	
-	- Uygulama adı ve şifrenin doğruluğu program içerisinde belirlene bir kullanıcı adı ve şifre ile yapılacaktır.
-	
-	- Uygulama genel düşünerek yazılmalıdır
-	
-	- ATM uygulamasının sürekli çalıştığı varsayımıyla yazılması önerilir
+	Sınıf Çalışması: Parametresi ile aldığı bir yazının sadece başındaki boşluk karakterlerini atan trimLeading ve
+ 	sadece sonundaki boşluk karakterlerini atan trimTrailing isimli metotları StringUtil sınıfı içerisinde yazınız ve 
+ 	aşağıdaki kod ile test ediniz
 ----------------------------------------------------------------------------------------------------------------------*/
 
 package orhn;
 
+import java.util.Iterator;
+
 class App {	
 	public static void main(String [] args)
-	{	
-		AtmApp.run(3);
+	{		
+		TrimLeadingTrailingTest.run();
 	}
 }
 
-class AtmApp{
-	
-	public static void doSucces(AtmCard atmCard, java.util.Scanner kb)
-	{
-		System.out.println("-----------------------------------");
-		System.out.println("Giriş başarılı");
-		System.out.println("-----------------------------------");
-	}
-	
-	public static void doFail(AtmCard atmCard, java.util.Scanner kb)
-	{
-		System.out.println("-----------------------------------");
-		System.out.println("Giriş başarısız. Artık deneme hakkınız bitti!....");
-		System.out.println("-----------------------------------");
-	}
-	
-	public static void doWorkForCard(AtmCard atmCard, int tryNum, java.util.Scanner kb)
-	{
-		int i = 0;
-		
-		for (;  i < tryNum; ++i) {
-			System.out.print("Kullanıcı adını giriniz : ");
-			String username = kb.nextLine();
-			System.out.print("Şifrenizi giriniz : ");
-			String password = kb.nextLine();
-			
-			if(atmCard.isvalid(username, password))
-				break;
-			
-			if(i != tryNum - 1)
-				System.out.printf("Giriş başarısız. %d giriş hakkınız kaldı!...%n", tryNum -1 - i);
-			
-		}
-		
-		if(i != tryNum)
-			doSucces(atmCard, kb);
-		else
-			doFail(atmCard, kb);
-		
-	}
-	
-	public static void run(int tryNum)
+class TrimLeadingTrailingTest {
+	public static void run()
 	{
 		java.util.Scanner kb = new java.util.Scanner(System.in);
 		
-		for(;;) {
-			System.out.println("Hoş geldiniz!...");
-			AtmCard atmCard = new AtmCard();
+		for (;;) {
+			System.out.print("Bir yazı giriniz:");
+			String s = kb.nextLine();		
 			
-			doWorkForCard(atmCard, tryNum, kb);
+			if ("elma".equals(s))
+				break;
+			
+			System.out.printf("(%s)%n", StringUtil.trimLeading(s));			
+			System.out.printf("(%s)%n", StringUtil.trimTrailing(s));			
 		}
+		
+		System.out.println("Tekrar yapıyor musunuz?");
 	}
 }
 
-
-class AtmCard{
-	public String username;
-	public String password;
-	
-	public AtmCard()
+class StringUtil{
+	public static String trimLeading(String str)
 	{
-		username = "orhan";
-		password = "2023";
+		int i;
+		int len = str.length();
+		
+		for (i = 0; i < len && Character.isWhitespace(str.charAt(i)); i++) 
+			;
+
+		return str.substring(i);
 	}
 	
-	public boolean isvalid(String uname, String passwd)
+	public static String trimTrailing(String str)
 	{
-		return uname.equals(username) && passwd.equals(password);
+		int i;
+		
+		for (i = str.length() - 1; i >= 0 && Character.isWhitespace(str.charAt(i)); --i) 
+			;
+
+		return str.substring(0, i + 1);
 	}
 }
+
 
